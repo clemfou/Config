b2e9fa0728600f0e1565fb55e6f2c76bae8bd8e0
clemfou <clemfou@email.com>
2024-10-09 11:03:53 +0200

    Nvim: add lsp autocmd for python files

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,6 +1,24 @@
+if vim.fn.executable('pylsp') == 1 then
+    -- if pylsp is installed
+    vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'python',
+        callback = function(args)
+            vim.lsp.start({
+                name = 'my-server-name',
+                cmd = {'pylsp', '--check-parent-process'},
+                -- Set the "root directory" to the parent directory of the file in the
+                -- current buffer (`args.buf`) that contains either a "setup.py" or a
+                -- "pyproject.toml" file. Files that share a root directory will reuse
+                -- the connection to the same LSP server.
+                root_dir = vim.fs.root(args.buf, {'setup.py', 'pyproject.toml'}),
+            })
+        end,
+    })
+end
+
 vim.api.nvim_create_autocmd('FileType', {
     desc = 'set a column at 80 chars',
-    pattern = {'markdown'},
+    pattern = 'markdown',
     group = utils.create_augroup('set_column'),
     callback = function()
         vim.opt_local.colorcolumn = '80'
